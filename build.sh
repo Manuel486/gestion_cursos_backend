0b3953b6ca8352ff6c3c9b35fff4cf749565a5eb
manuel <manuelcapricornio12@hotmail.com>
2025-04-06 20:53:47 -0500

    creating user

--- build.sh
+++ build.sh
@@ -1,8 +1,15 @@
-
+#!/bin/bash
 set -o errexit
 
+# Instalar dependencias
 pip install -r requirements.txt
 
+# Recolectar archivos estáticos
 python manage.py collectstatic --no-input
 
+# Ejecutar migraciones
 python manage.py migrate
+
+# Crear el superusuario automáticamente
+echo "Creating superuser..."
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('testUser123', 'testuser@example.com', 'Test@1234')" | python manage.py shell
